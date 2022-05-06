96e71b11b2563e799a77b9a993811c6a79b56a51
Andrew Roe <70938156+AndrewRoe34@users.noreply.github.com>
2022-05-06 17:33:09 -0400

    [Implement] Added iterative binary search method

--- C02-Getting-Started/code/binary_search.py
+++ C02-Getting-Started/code/binary_search.py
@@ -1,15 +1,28 @@
-def binary_search(arr, low, high, x):
+def binary_recursive_search(arr, low, high, x):
     if low > high:
         return -1
     mid = (low + high) // 2
     if arr[mid] == x:
         return mid
     elif arr[mid] > x:
-        return binary_search(arr, low, mid - 1, x)
+        return binary_recursive_search(arr, low, mid - 1, x)
     else:
-        return binary_search(arr, mid + 1, high, x)
+        return binary_recursive_search(arr, mid + 1, high, x)    
+
+    
+def binary_iterative_search(arr, low, high, x):
+    while len(arr) > 0 and low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] > x:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return -1
 
 
 arr = [1, 2, 3, 4, 5]
 
-print(binary_search(arr, 0, len(arr) - 1, 2))
+print(binary_recursive_search(arr, 0, len(arr) - 1, 2))
+print(binary_iterative_search(arr, 0, len(arr) - 1, 4))
